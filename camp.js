93704f7dd0aafa5888d40c70bbb4d557bc7d4506
本間敬太 <keitakunv2@gmail.com>
2023-10-30 00:35:09 +0900

    Initial Commit

---
+++ camp.js
@@ -0,0 +1,22 @@
+jQuery(function($){
+  $(".hoge").click(function(){
+      $(this).css("background-color","blue")
+      $(this).val("検索中・・・")
+  });
+});
+
+  console.log(myVar);
+
+  const sortableTable = new SortableTable();
+  // set table element
+  sortableTable.setTable(document.querySelector('#my-table1'));
+  // set data to be sorted
+  sortableTable.setData(myVar);
+  // handling events
+  sortableTable.events()
+      .on('sort', (event) => {
+        console.log(`[SortableTable#onSort]
+      event.colId=${event.colId}
+      event.sortDir=${event.sortDir}
+      event.data=\n${JSON.stringify(event.data)}`);
+      });
